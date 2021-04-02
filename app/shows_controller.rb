c835970e35d2a2d2ce08012d15f8f99c4a9cff9c
MarkBezsonov <markbezsonov@gmail.com>
2021-04-01 23:37:16 -0500

    Done.

---
+++ app/shows_controller.rb
@@ -0,0 +1,38 @@
+def instantiate_and_save
+    show = Show.new
+    show.name = "This is a name."
+    movie.save 
+  end
+  
+  def create
+    attributes = {
+        name: "Name",
+        network: "Network",
+        day: "Day",
+        rating: 0,
+        season: "Season"
+    }
+    show = Show.create(attributes)
+  end
+
+def highest_rating(rating)
+    Show.maximum(rating)
+end
+
+def most_popular_show
+end
+
+def lowest_rating
+end
+
+def least_popular_show
+end
+
+def ratings_sum
+end
+
+def popular_shows
+end
+
+def shows_by_alphabetical_order
+end
