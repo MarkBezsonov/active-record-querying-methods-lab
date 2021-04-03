1f966f0184db77e5c5e9b36ab3aa0629896a1546
MarkBezsonov <markbezsonov@gmail.com>
2021-04-03 00:59:43 -0500

    commit tree a77cc5f4b60c432c9e84cf31e451bb2eff8a1973

--- app/models/show.rb
+++ app/models/show.rb
@@ -20,16 +20,16 @@ class Show < ActiveRecord::Base
         Show.maximum(:rating)
     end
     
-    def self.most_popular_show #Doesn't work
-        Show.where(:rating == self.highest_rating)
+    def self.most_popular_show
+        self.where("rating = ?", self.highest_rating).first
     end
     
     def self.lowest_rating
         Show.minimum(:rating)
     end
     
-    def self.least_popular_show #Doesn't work
-        
+    def self.least_popular_show
+        self.where("rating = ?", self.lowest_rating).first
     end
     
     def self.ratings_sum
@@ -37,10 +37,10 @@ class Show < ActiveRecord::Base
     end
     
     def self.popular_shows #Doesn't work
-        Show.where(rating: true).any?
+        self.where("rating >= 5")
     end
 
-    def self.shows_by_alphabetical_order #Doesn't work
-        Show.order(:created_at)
+    def self.shows_by_alphabetical_order
+        Show.order(:name)
     end
 end
